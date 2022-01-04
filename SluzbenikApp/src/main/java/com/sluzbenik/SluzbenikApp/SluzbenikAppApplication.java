2599fce844db7071af460b6afe5ce9aabcc7d54e
Bojan <bojanbaskalo@hotmail.rs>
2022-01-04 21:47:53 +0100

    Spring SluzbenikApp initialization + schemas

---
+++ SluzbenikApp/src/main/java/com/sluzbenik/SluzbenikApp/SluzbenikAppApplication.java
@@ -0,0 +1,13 @@
+package com.sluzbenik.SluzbenikApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SluzbenikAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SluzbenikAppApplication.class, args);
+	}
+
+}
