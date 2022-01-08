dd849ed74d00905a93d052f5cf41363d13c233e8
darkotica <58399701+darkotica@users.noreply.github.com>
2022-01-08 19:19:18 +0100

    Added forgotten files.

---
+++ ImunizacijaApplication/ImunizacijaApp/src/main/java/com/imunizacija/ImunizacijaApp/ImunizacijaAppApplication.java
@@ -0,0 +1,13 @@
+package com.imunizacija.ImunizacijaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImunizacijaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImunizacijaAppApplication.class, args);
+	}
+
+}
---
+++ ImunizacijaApplication/ImunizacijaApp/src/test/java/com/imunizacija/ImunizacijaApp/ImunizacijaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imunizacija.ImunizacijaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImunizacijaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
