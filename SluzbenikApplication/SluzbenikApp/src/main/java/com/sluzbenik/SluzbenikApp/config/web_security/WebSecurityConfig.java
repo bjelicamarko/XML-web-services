6ce2cd1929d4d0991d91b59eadb7496b53083c06
darkotica <58399701+darkotica@users.noreply.github.com>
2022-02-18 21:16:22 +0100

    Quick fix for rdf and dzs view for citizen

--- SluzbenikApplication/SluzbenikApp/src/main/java/com/sluzbenik/SluzbenikApp/config/web_security/WebSecurityConfig.java
+++ SluzbenikApplication/SluzbenikApp/src/main/java/com/sluzbenik/SluzbenikApp/config/web_security/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/socket/**").permitAll()
 				.antMatchers("/api/users/login").permitAll()
 				.antMatchers("/api/dzs/odKorisnika/**").permitAll() //todo izbrisati
+				.antMatchers("/api/dzs/generatePDF/**").permitAll() //todo izbrisati
 				.antMatchers("/api/users/test-create-sluzbenik").permitAll() //todo izbrisati
 				.antMatchers("/api/sistemski-magacin/dobaviTermin").permitAll() // Komunikacija beckova
 				.antMatchers("/api/izvestaji/dobaviIzvestaje/*").permitAll() // Komunikacija beckova
