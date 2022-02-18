bcf1fdc8c6096649323d5060aa84429c0bf164a8
darkotica <58399701+darkotica@users.noreply.github.com>
2022-02-18 18:08:41 +0100

    Fixed bug with display of refenced documents

--- SluzbenikApplication/SluzbenikAppFront/sluzbenik/src/modules/search/pages/search-page/search-page.component.ts
+++ SluzbenikApplication/SluzbenikAppFront/sluzbenik/src/modules/search/pages/search-page/search-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DocumentProviderService } from 'src/modules/shared/services/document-provider.service';
 import { SnackBarService } from 'src/modules/shared/services/snack-bar.service';
 import { UtilService } from 'src/modules/shared/services/util.service';
+import { RespDTO } from '../../models/new_dto_models/resp-dto';
 import { SearchResults } from '../../models/search-results';
 import { SearchService } from '../../services/search-service';
 
@@ -14,14 +15,14 @@ import { SearchService } from '../../services/search-service';
 export class SearchPageComponent implements OnInit {
 
   registrationFormGroup: FormGroup;
-  searchResults: SearchResults | undefined;
+  searchResults: RespDTO | undefined;
 
   refs: string;
   refBy: string;
 
   constructor(private fb: FormBuilder, private snackBarService: SnackBarService,
-              private searchService: SearchService, private utilService: UtilService,
-              private documentProviderService: DocumentProviderService) { 
+    private searchService: SearchService, private utilService: UtilService,
+    private documentProviderService: DocumentProviderService) {
 
     this.registrationFormGroup = this.fb.group({
       documentType: ['Saglasnost', Validators.required],
@@ -58,63 +59,81 @@ export class SearchPageComponent implements OnInit {
     let documentType: string = this.registrationFormGroup.get('documentType')?.value;
 
     this.searchService.basicSearch(userId, searchText, documentType.toLowerCase()).subscribe((response) => {
-      let res = this.searchService.parseXmlArrayFalse(response.body as string).Search_results;
+      let res = this.searchService.parseXml(response.body as string);
 
-      if(res.Search_results !== '') {
+      if (res.Search_results !== '') {
         this.searchResults = res;
         this.handleHtmlRef(documentType);
       }
       else {
         this.searchResults = undefined;
       }
-      
-      if(!this.searchResults) 
+
+      if (!this.searchResults)
         this.snackBarService.openSnackBarFast("Nema rezultata za unetu pretragu");
 
       console.log(this.searchResults);
     },
-    (error) => {
-      this.snackBarService.openSnackBarFast("Doslo je do greške prilikom pretrage..");
-    })
+      (error) => {
+        this.snackBarService.openSnackBarFast("Doslo je do greške prilikom pretrage..");
+      })
   }
+
+  getPdfOnLink(refby: boolean, documentId: string) {
+    let documentName: string = "";
+    if (refby) {
+      documentName = this.refBy.toLowerCase();
+    } else {
+      documentName = this.refs.toLowerCase();
+    }
+
+    this.documentProviderService.getDocumentPDF(documentName, documentId).subscribe((response) => {
+      if (response.body)
+        this.utilService.downloadPDFDocument(response.body, documentName);
+    },
+      (error) => {
+        this.snackBarService.openSnackBarFast("Doslo je do greške prilikom preuzimanja/prikazivanja dokumenta.");
+      });
+  }
+
 
   getPdf(documentId: any) {
     let documentName: string = this.registrationFormGroup.get('documentType')?.value.toLowerCase();
 
     this.documentProviderService.getDocumentPDF(documentName, documentId).subscribe((response) => {
-      if(response.body)
+      if (response.body)
         this.utilService.downloadPDFDocument(response.body, documentName);
-    }, 
-    (error) => {
-      this.snackBarService.openSnackBarFast("Doslo je do greške prilikom preuzimanja/prikazivanja dokumenta.");
-    });
+    },
+      (error) => {
+        this.snackBarService.openSnackBarFast("Doslo je do greške prilikom preuzimanja/prikazivanja dokumenta.");
+      });
   }
-  
+
   getHtml(documentId: any) {
     let documentName: string = this.registrationFormGroup.get('documentType')?.value.toLowerCase();
 
     this.documentProviderService.getDocumentHTML(documentName, documentId).subscribe((response) => {
-      if(response.body)
+      if (response.body)
         this.utilService.openHtmlDocumentInNewTab(response.body);
-    }, 
-    (error) => {
-      this.snackBarService.openSnackBarFast("Doslo je do greške prilikom prikazivanja dokumenta.");
-    });
+    },
+      (error) => {
+        this.snackBarService.openSnackBarFast("Doslo je do greške prilikom prikazivanja dokumenta.");
+      });
   }
 
   private handleHtmlRef(documentType: string) {
-    if(documentType === 'Saglasnost') {
+    if (documentType === 'Saglasnost') {
       this.refs = "Interesovanje";
       this.refBy = "Potvrda";
-    } 
-    else if(documentType == "Potvrda") {
+    }
+    else if (documentType == "Potvrda") {
       this.refs = "Saglasnost";
       this.refBy = "DZS";
-    } 
-    else if(documentType == "DZS") {
+    }
+    else if (documentType == "DZS") {
       this.refs = "Potvrda";
       this.refBy = "Zahtev DZS";
-    } 
+    }
   }
 
 }
--- SluzbenikApplication/SluzbenikAppFront/sluzbenik/src/modules/search/services/search-service.ts
+++ SluzbenikApplication/SluzbenikAppFront/sluzbenik/src/modules/search/services/search-service.ts
@@ -8,9 +8,9 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 })
 export class SearchService {
 
-  private headers = new HttpHeaders({ "Content-Type": "application/xml", "Accept" : "application/xml"});
+  private headers = new HttpHeaders({ "Content-Type": "application/xml", "Accept": "application/xml" });
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   basicSearch(userId: string, searchText: string, documentType: string): Observable<HttpResponse<string>> {
     let queryParams = {}
@@ -23,19 +23,40 @@ export class SearchService {
     return this.http.get<HttpResponse<string>>(`indirekcija/api/${documentType}/search?userId=${userId}&searchText=${searchText}`, queryParams);
   }
 
-  parseXml(xmlStr: string) : any { // { explicitArray: false }
+  parseXml(xmlStr: string): any { // { explicitArray: false }
     var result;
     var parser = require('xml2js');
-    parser.Parser().parseString(xmlStr, (e: any, r: any) => {result = r});
+    parser.Parser().parseString(xmlStr, (e: any, r: any) => { result = r });
     return result;
-  } 
+  }
 
-  parseXmlArrayFalse(xmlStr: string) : any { // { explicitArray: false }
+  parseXmlArrayFalse(xmlStr: string): any { // { explicitArray: false }
     var result;
     var parser = require('xml2js');
-    parser.Parser({ explicitArray: false }).parseString(xmlStr, (e: any, r: any) => {result = r});
+    parser.Parser({ explicitArray: false }).parseString(xmlStr, (e: any, r: any) => { result = r });
     return result;
-  } 
+  }
+
+  returnDummy2(): string {
+    return `<Search_results>
+    <Search_result>
+        <Document_id>4125125</Document_id>
+        <Referencing>
+            <Document_id>2312312</Document_id>
+        </Referencing>
+        <Referenced_by>
+            <Document_id>3</Document_id>
+        </Referenced_by>
+    </Search_result>
+    <Search_result>
+        <Document_id>7654321</Document_id>
+        <Referencing/>
+        <Referenced_by>
+            <Document_id>3</Document_id>
+        </Referenced_by>
+    </Search_result>
+  </Search_results>`;
+  }
 
   returnDumy(): string {
     return `
