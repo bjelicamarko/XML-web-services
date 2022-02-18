cd9c708fcd8261a11dbd5ea7f6896ef40a42c816
darkotica <58399701+darkotica@users.noreply.github.com>
2022-02-18 19:16:46 +0100

    Added references for metadata

--- ImunizacijaApplication/ImunizacijaApp/src/main/java/com/imunizacija/ImunizacijaApp/controllers/MetapodaciController.java
+++ ImunizacijaApplication/ImunizacijaApp/src/main/java/com/imunizacija/ImunizacijaApp/controllers/MetapodaciController.java
@@ -1,6 +1,8 @@
 package com.imunizacija.ImunizacijaApp.controllers;
 
 import com.imunizacija.ImunizacijaApp.model.dto.comunication_dto.AdvancedSearchResults;
+import com.imunizacija.ImunizacijaApp.model.dto.comunication_dto.SearchResult;
+import com.imunizacija.ImunizacijaApp.model.dto.comunication_dto.SearchResults;
 import com.imunizacija.ImunizacijaApp.model.vakc_sistem.korisnik.Korisnik;
 import com.imunizacija.ImunizacijaApp.service.MetadataService;
 import com.imunizacija.ImunizacijaApp.service.MetadataServiceImpl;
@@ -23,10 +25,10 @@ public class MetapodaciController {
     MetadataService metadataService;
 
     @GetMapping(value = "/pretraga/{query}", produces = MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<AdvancedSearchResults> advancedSearch(@PathVariable String query) {
+    public ResponseEntity<SearchResults> advancedSearch(@PathVariable String query) {
         try{
-            List<String> res = metadataService.getDocIdsFromQuery(query);
-            return new ResponseEntity<>(new AdvancedSearchResults(res), HttpStatus.OK);
+            SearchResults res = metadataService.getDocIdsFromQuery(query);
+            return new ResponseEntity<>(res, HttpStatus.OK);
         }catch (Exception e){
             System.out.println(e.getMessage());
             e.printStackTrace();
